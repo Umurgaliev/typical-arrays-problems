2e8ad6ff116f13fb517b9105dfa335a25392a00f
Umurgaliev <70776862+Umurgaliev@users.noreply.github.com>
2021-03-08 17:42:50 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array ? array.reduce((min, current) => {if (current < min) min = current; return min;}, 0) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array ? array.reduce((max, current) => {if (current > max) max = current; return max;}, 0) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array !== undefined && array.length > 0 ? array.reduce((sum, current) => sum + current, 0) / array.length : 0;
 }
